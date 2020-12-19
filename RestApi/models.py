9475839b57262349ca7cc1bda3b569af06ec3bca
owais <owais.niaz@targetofs.com>
2020-12-19 18:32:06 +0500

    Initial Commit

---
+++ RestApi/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+class UserRegitration(models.Model):
+   # ID = models.AutoField(primary_key=True),
+    Name = models.CharField(max_length=30),
+    email = models.EmailField(max_length=40)
+    password = models.CharField(max_length=20)
+    confirmpassword=models.CharField(max_length=20)
+    def __str__(self):
+        return self.Name

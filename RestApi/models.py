0e78c13f0ebe51aeecd750785b790cccc9a11aad
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-02-14 21:49:07 +0500

    Update models.py

--- RestApi/models.py
+++ RestApi/models.py
@@ -4,6 +4,7 @@ class UserRegitration(models.Model):
     Name = models.CharField(max_length=30),
     email = models.EmailField(max_length=40)
     password = models.CharField(max_length=20)
+      
     confirmpassword=models.CharField(max_length=20)
     def __str__(self):
         return self.Name

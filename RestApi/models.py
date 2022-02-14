56bc0010dc4cda038542694f0d38a73fbdb9c103
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-02-14 21:49:20 +0500

    Update models.py

--- RestApi/models.py
+++ RestApi/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 class UserRegitration(models.Model):
    # ID = models.AutoField(primary_key=True),
+   
     Name = models.CharField(max_length=30),
     email = models.EmailField(max_length=40)
     password = models.CharField(max_length=20)
